05a6e1c58d5fb209f64e6ca3bc8e0c39d5ab7a81
ateucher <andy.teucher@gmail.com>
2013-01-31 14:04:59 -0800

    Added the (commented out) function to source a script on github

--- func.r
+++ func.r
@@ -73,3 +73,23 @@ cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
 
 # The palette with black:
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+################################################################################
+
+# source_https <- function(url, ...) {
+#   # Source a script from a https (e.g., github) server.  From Tony Breyal here:
+#   # http://tonybreyal.wordpress.com/2011/11/24/source_https-sourcing-an-r-script-from-github/
+#   # I have this in my Rprofile.site
+#   # load package
+#   require(RCurl)
+#   
+#   # parse and evaluate each .R script
+#   sapply(c(url, ...), function(u) {
+#     eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
+#   })
+# }
+# 
+# ## Example
+# # source_https("https://raw.github.com/tonybreyal/Blog-Reference-Functions/master/R/bingSearchXScraper/bingSearchXScraper.R",
+# #              "https://raw.github.com/tonybreyal/Blog-Reference-Functions/master/R/htmlToText/htmlToText.R")
+
